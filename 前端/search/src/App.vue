45206ed071952933f2a9351c34cf0a234428a9e8
atao <87337334@qq.com>
2022-06-25 15:54:30 +0800

    feat: build front

---
+++ 前端/search/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+ /* #app{
+   margin-top: 100%;
+ } */
+</style>
+
+<script>
+
+
+export default {
+  name: 'App',
+}
+</script>
